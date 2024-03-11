eb14222f72d338035be7fb35787ac15d7eb4cea7
St-Luciferr <santosh.762417@thc.tu.edu.np>
2024-03-11 10:32:07 +0545

    contrastive loss update

--- siamese_net/contrastive.py
+++ siamese_net/contrastive.py
@@ -12,8 +12,8 @@ class ContrastiveLoss(nn.Module):
     def forward(self, output1, output2, label):
         euclidean_distance = F.pairwise_distance(output1, output2)
         loss_contrastive = torch.mean(
-            (1 - label) * torch.pow(euclidean_distance, 2)
-            + (label)
+            (label) * torch.pow(euclidean_distance, 2)
+            + (1-label)
             * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2)
         )
 
